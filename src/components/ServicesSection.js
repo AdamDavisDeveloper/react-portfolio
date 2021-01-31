5383be2ad12b401c2d6eb709b26adcb4eacc6de4
Adam Edward <adavismarketer@gmail.com>
2021-01-30 23:37:15 -0500

    Services Section

---
+++ src/components/ServicesSection.js
@@ -0,0 +1,60 @@
+//Import Icons
+import clock from "../img/clock.svg";
+import diaphragm from "../img/diaphragm.svg";
+import money from "../img/money.svg";
+import teamwork from "../img/teamwork.svg";
+//Import Images
+import home2 from "../img/home2.png";
+
+const ServicesSection = () => {
+  return (
+    <div className="services">
+      <div className="description">
+        <h2>
+          High <span>quality</span> services
+        </h2>
+        <div className="cards">
+          <div className="card">
+            <div className="icon">
+              <img src={clock} alt="icon" />
+              <h3>Efficient</h3>
+            </div>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+        </div>
+        <div className="cards">
+          <div className="card">
+            <div className="icon">
+              <img src={teamwork} alt="icon" />
+              <h3>Teamwork</h3>
+            </div>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+        </div>
+        <div className="cards">
+          <div className="card">
+            <div className="icon">
+              <img src={diaphragm} alt="icon" />
+              <h3>Diagragm</h3>
+            </div>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+        </div>
+        <div className="cards">
+          <div className="card">
+            <div className="icon">
+              <img src={money} alt="icon"/>
+              <h3>Affordable</h3>
+            </div>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+        </div>
+      </div>
+      <div className="image">
+          <img src={home2} alt="camera"/>
+      </div>
+    </div>
+  );
+};
+
+export default ServicesSection;
--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,10 +1,14 @@
 //Import Components
 import AboutSection from '../components/AboutSection';
+import ServicesSection from '../components/ServicesSection';
 
 const AboutUs = () => {
     return (
+        <>
         <AboutSection />
-    )
-}
+        <ServicesSection />
+        </>
+    );
+};
 
 export default AboutUs;
