7b4e1bc9af8fdc2f980284e65bd035e92d1d5664
Adam Edward <adavismarketer@gmail.com>
2021-02-02 07:09:30 -0500

    Styled FAQ section

--- src/components/FaqSection.js
+++ src/components/FaqSection.js
@@ -1,6 +1,10 @@
+//Styled Components
+import styled from 'styled-components';
+import {About} from '../styles';
+
 const FaqSection = () => {
   return (
-    <div className="faq">
+    <Faq>
       <h2>
         Any Questions <span>FAQ</span>
       </h2>
@@ -13,6 +17,7 @@ const FaqSection = () => {
             labore.
           </p>
         </div>
+        <div className="faq-line"></div>
       </div>
       <div className="question">
         <h4>Daily Schedule</h4>
@@ -23,6 +28,7 @@ const FaqSection = () => {
             labore.
           </p>
         </div>
+        <div className="faq-line"></div>
       </div>
       <div className="question">
         <h4>Daily Payments?</h4>
@@ -33,6 +39,7 @@ const FaqSection = () => {
             labore.
           </p>
         </div>
+        <div className="faq-line"></div>
       </div>
       <div className="question">
         <h4>What products do you offer?</h4>
@@ -43,9 +50,37 @@ const FaqSection = () => {
             labore.
           </p>
         </div>
+        <div className="faq-line"></div>
       </div>
-    </div>
+    </Faq>
   );
 };
+
+const Faq = styled(About)`
+  display: block;
+  span {
+    display: block
+  }
+  h2 {
+    padding-bottom: 2rem;
+    font-weight: lighter;
+  }
+  .faq-line {
+    background: #cccc;
+    height: 0.2rem;
+    margin: 2rem 0rem;
+    width: 100%;
+  }
+  .question {
+    padding: 3rem 0rem;
+    cursor: pointer;
+  }
+  .answer {
+    padding: 2rem 0rem;
+    p {
+      padding: 1rem 0rem;
+    }
+  }
+`;
 
 export default FaqSection;
