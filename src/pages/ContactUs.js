8fcc7b1044e0c11611f427bcfff1fe9aa97faacd
Adam Davis <adavisdeveloper@gmail.com>
2021-04-17 23:20:44 -0400

    change us to me

--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -24,7 +24,7 @@ const ContactUs = () => {
         <Hide>
           <Social variants={titleAnim}>
             <Circle />
-            <h2>Send Us A Message</h2>
+            <h2>Send Me A Message</h2>
           </Social>
         </Hide>
         <Hide>
